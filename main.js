e27972c59929574c7b67cf97a54e2fdf04c5667b
Junaid Ahmed Shaikh <117148118+junaidahmedshaikh@users.noreply.github.com>
2023-10-18 17:11:06 +0530

    Pushing

---
+++ main.js
@@ -0,0 +1,15 @@
+// var body = document.getElementsByTagName('body');
+//         var lightMode = docutment.getElementsById('lightMode');
+
+//         lightMode.onclick = function (){
+//             if (body.className == "lightMode" ){
+//                 body.className = "dark-theme";
+//             } else{
+//                 body.className = "body";
+//             }
+//         }
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-theme");
+  }
